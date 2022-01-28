a8d51dde1089750f97067b86926c25cb42282870
Samuel Ronce <sam@webcreative5.net>
2022-01-28 19:11:07 +0100

    3.0.0-beta.4

--- tests/rpg.spec.ts
+++ tests/rpg.spec.ts
@@ -1,11 +1,15 @@
 import { RpgWorld, RpgPlayer } from '@rpgjs/server'
-import { testing } from '@rpgjs/testing'
-import modules from '../src/modules'
+import { testing, clear } from '@rpgjs/testing'
+import main from '../src/modules/main'
+import defaultGui from '@rpgjs/default-gui' 
 
 let player: RpgPlayer
 
 beforeEach(async () => {
-    const fixture = testing(modules, {
+    const fixture = testing([
+        main,
+        defaultGui
+    ], {
         basePath: __dirname + '/../'
     })
     const client = await fixture.createClient()
@@ -14,4 +18,8 @@ beforeEach(async () => {
 
 test('test player', () => {
     expect(player).toBeDefined()
+})
+
+afterEach(() => {
+    clear()
 })

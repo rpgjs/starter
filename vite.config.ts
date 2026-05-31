5df5969d62d438a1b6e43eb37e375d09183ae7a5
RSamaium <sam@newbot.io>
2026-05-31 16:03:44 +0200

    Fix Pixi Vite deps and player name assignment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { rpgjs, tiledMapFolderPlugin } from '@rpgjs/vite';
 import startServer from './src/server';
 
 export default defineConfig({
+  optimizeDeps: {
+    include: ['parse-svg-path', '@xmldom/xmldom']
+  },
   plugins: [
     tiledMapFolderPlugin({
       sourceFolder: './src/tiled',      // Folder containing your TMX files
